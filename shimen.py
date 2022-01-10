d209906c9fd315c1e185a7c63588efefaa2e7521
a631069724 <631069724@qq.com>
2022-01-10 11:46:05 +0800

    init

--- shimen.py
+++ shimen.py
@@ -6,3 +6,11 @@ from base import *
 
 class Shimen(Base):
     EventShimenRenwu=Event('../pic/shimen/shimen_renwu.png',Task,(1140,224))
+
+    def run(self):
+        pos=match_sub_image_in_rect(self.screenShot(),
+            self.EventShimenRenwu.Img,
+            self.EventShimenRenwu.Rect)
+        #师门任务
+        if pos:
+            self.click(*self.EventShimenRenwu.Click)
