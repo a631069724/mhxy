4dec0bbd5ce66ac74d653d4e6f9c51b8931c1820
a631069724 <631069724@qq.com>
2022-01-11 11:42:22 +0800

    update

--- base.py
+++ base.py
@@ -41,6 +41,42 @@ def match(img1,img2):
         return pt
     return None
 
+def Rotation(img,angle=90):
+
+    height, width = img.shape[:2]
+    
+    matRotate = cv2.getRotationMatrix2D((height * 0.5, width * 0.5), angle, 1)
+    dst = cv2.warpAffine(img, matRotate, (width, height*2))
+    rows, cols = dst.shape[:2]
+    
+    for col in range(0, cols):
+        if dst[:, col].any():
+            left = col
+            break
+    
+    for col in range(cols-1, 0, -1):
+        if dst[:, col].any():
+            right = col
+            break
+    
+    for row in range(0, rows):
+        if dst[row,:].any():
+            up = row
+            break
+    
+    for row in range(rows-1,0,-1):
+        if dst[row,:].any():
+            down = row
+            break
+    
+    res_widths = abs(right - left)
+    res_heights = abs(down - up)
+    res = np.zeros([res_heights ,res_widths, 3], np.uint8)
+    
+    for res_width in range(res_widths):
+        for res_height in range(res_heights):
+            res[res_height, res_width] = dst[up+res_height, left+res_width]
+    return res
 
 
 class Device():
